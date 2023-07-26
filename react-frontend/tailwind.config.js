90c1d9de05f84b9a80002d131a589a671cea8fc9
Paul Chester <paulvillanueva2012@gmail.com>
2023-07-27 02:35:43 +0800

    [react] Update packages, minor Tailwind config

--- react-frontend/tailwind.config.js
+++ react-frontend/tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pokemon: ["Pokemon", "serif"],
+      },
+      animation: {
+        border: "border 5s ease infinite",
+      },
+      keyframes: {
+        border: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
